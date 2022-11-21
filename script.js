a14dc249d9da1543818f3f6b92981baf2df5c827
Elian E. Soto Ramos <elian.0617@gmail.com>
2022-11-21 19:14:01 -0400

    optimization

--- script.js
+++ script.js
@@ -28,9 +28,7 @@ const header = document.querySelector("[data-header]");
 window.addEventListener("scroll", function () {
   if (window.scrollY >= 400) {
     header.classList.add("active");
-    goTopBtn.classList.add("active");
   } else {
     header.classList.remove("active");
-    goTopBtn.classList.remove("active");
   }
 });
